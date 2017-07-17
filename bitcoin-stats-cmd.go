37212175e800a91d5f5dc21fa9cead4d672332d5
kyluke <1087345+kyluke@users.noreply.github.com>
2017-07-17 07:29:04 +0200

    - Added support for config files  - Added Kraken support for USD, EUR and GBP  - Replaced FATAL with PRINTLN statements to prevent crashes

--- bitcoin-stats-cmd.go
+++ bitcoin-stats-cmd.go
@@ -4,7 +4,9 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/Beldur/kraken-go-api-client"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/spf13/viper"
 	"log"
 	"net/http"
 	"os"
@@ -37,6 +39,20 @@ type Bitstamp struct {
 	Open      string `json:"open"`
 }
 
+// Config type
+type Config struct {
+	LogFile        string
+	SqliteLocation string
+	Kraken         KrakenConfig
+}
+
+type KrakenConfig struct {
+	APIKey    string
+	APISecret string
+}
+
+var config Config
+
 // Setup global home and config folders
 // File location
 var fileLocation string = "/.config/bitcoin-stats/"
@@ -52,6 +68,8 @@ func bitcoin_prices() {
 		luno_ticker()
 		// Start bitstamp ticker
 		bitstamp_ticker()
+		// Start kraken ticker
+		kraken_ticker()
 
 		time.Sleep(11 * time.Minute)
 	}
@@ -106,6 +124,44 @@ func bitstamp_ticker() {
 	insert_into_sqlite("Bitstamp", record.Timestamp, record.Ask, record.Bid, record.Volume, "USD")
 }
 
+// Gets ticker data from kraken
+func kraken_ticker() {
+	// If the API keys are not present, just return
+	if len(config.Kraken.APIKey) == 0 && len(config.Kraken.APISecret) == 0 {
+		return
+	}
+
+	api := krakenapi.New(config.Kraken.APIKey, config.Kraken.APISecret)
+
+	// There are also some strongly typed methods available
+	tickerEUR, err := api.Ticker(krakenapi.XXBTZEUR)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Insert into SQlite
+	insert_into_sqlite("Kraken", strconv.FormatInt(int64(time.Now().Unix()), 10), tickerEUR.XXBTZEUR.Ask[0], tickerEUR.XXBTZEUR.Bid[0], tickerEUR.XXBTZEUR.Volume[0], "EUR")
+
+	// There are also some strongly typed methods available
+	tickerUSD, err := api.Ticker(krakenapi.XXBTZUSD)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Insert into SQlite
+	insert_into_sqlite("Kraken", strconv.FormatInt(int64(time.Now().Unix()), 10), tickerUSD.XXBTZUSD.Ask[0], tickerUSD.XXBTZUSD.Bid[0], tickerUSD.XXBTZUSD.Volume[0], "USD")
+
+	// There are also some strongly typed methods available
+	tickerGBP, err := api.Ticker(krakenapi.XXBTZGBP)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Insert into SQlite
+	insert_into_sqlite("Kraken", strconv.FormatInt(int64(time.Now().Unix()), 10), tickerGBP.XXBTZGBP.Ask[0], tickerGBP.XXBTZGBP.Bid[0], tickerGBP.XXBTZGBP.Volume[0], "GBP")
+
+}
+
 // performs an API call to a URL and returns a JSON body response
 func api_call(urlRequest string) *http.Response {
 
@@ -114,7 +170,7 @@ func api_call(urlRequest string) *http.Response {
 	// Build the request
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Fatal("NewRequest: ", err)
+		log.Println("NewRequest: ", err)
 		return nil
 	}
 
@@ -129,7 +185,7 @@ func api_call(urlRequest string) *http.Response {
 	// returns an HTTP response
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal("Do: ", err)
+		log.Println("Do: ", err)
 		return nil
 	}
 
@@ -153,17 +209,25 @@ func check(e error) {
 func sqlite_open() *sql.DB {
 	db, err := sql.Open("sqlite3", home+"/data.db")
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 	return db
 }
 
 // Sets up the sqlite databases and connections
 func setup_sqlite_db() {
-	// os.Remove(home + "/data.db")
+	// Setup sqlite connection
+	var sqliteConnection string
+
+	// If the config file line is empty, then default
+	if len(config.SqliteLocation) > 0 {
+		sqliteConnection = config.SqliteLocation
+	} else {
+		sqliteConnection = home + "/data.db"
+	}
 	// Check if the sqlite database already exists, if it does not, continue
 	// else, don't care
-	if _, err := os.Stat(home + "/data.db"); os.IsNotExist(err) {
+	if _, err := os.Stat(sqliteConnection); os.IsNotExist(err) {
 		sqliteDB := sqlite_open()
 
 		sqlStmt := `create table exchanges (id integer not null primary key, exchange text, timestamp real, ask real, bid real, volume real default 0, currencyCode text);`
@@ -191,10 +255,48 @@ func insert_into_sqlite(exchange string, timestamp string, ask string, bid strin
 	sqliteDB.Close()
 }
 
+// Configure configs
+func config_init() {
+
+	// Config File
+	viper.SetConfigName("config") // no need to include file extension
+	viper.AddConfigPath(home)     // set the path of your config file
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Println("Config file not found... Error %s\n", err)
+	} else {
+
+		// ========= CONFIG ================================================================
+		logFile := viper.GetString("config.logFile")
+		sqliteLocation := viper.GetString("config.sqliteLocation")
+		krakenAPIKey := viper.GetString("exchanges.kraken.apiKey")
+		krakenAPISecret := viper.GetString("exchanges.kraken.apiSecret")
+
+		// Kraken
+		kraken := KrakenConfig{
+			APIKey:    krakenAPIKey,
+			APISecret: krakenAPISecret,
+		}
+
+		// Main Config
+		config = Config{
+			LogFile:        logFile,
+			SqliteLocation: sqliteLocation,
+			Kraken:         kraken,
+		}
+	}
+
+}
+
 func main() {
+
+	// Initialise config file and settings
+	config_init()
+
 	// Logging
 	// open a file
-	f, err := os.OpenFile("/tmp/bitcoin-stats.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	f, err := os.OpenFile(config.LogFile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
 	if err != nil {
 		log.Printf("error opening file: %v", err)
 	}
@@ -204,9 +306,6 @@ func main() {
 
 	// assign it to the standard logger
 	log.SetOutput(f)
-
-	// Initialise config file and settings
-	// config_init()
 
 	// Setup Sqlite DB
 	setup_sqlite_db()
